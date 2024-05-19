c64f69be89b6874c895073f78d91987e5688fa88
Wai Phyo Aung <waung0248@gmail.com>
2024-05-20 00:07:53 +0630

    CRUD for Author

--- BookLibraryDTO/Model/BookEntity.cs
+++ BookLibraryDTO/Model/BookEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,16 @@ namespace BookLibraryDTO.Model
     public class BookEntity
     {
         public int id { get; set; }
+        [Required]
         public string name { get; set; }
         public string coverPhoto { get; set; }
+        [Required]
         public string bookType { get; set; }
+        [Required]
         public string description { get; set; }
         public int author_id { get; set; }
         public DateTime created_date { get; set; }
-        public DateTime updated_date { get; set; }
+        public DateTime? updated_date { get; set; }
         public bool is_delete { get; set; }
     }
 }
